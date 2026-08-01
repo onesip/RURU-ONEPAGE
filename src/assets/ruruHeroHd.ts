74616cc1c02d63fe8f5f112c6f4595f4188981db
onesip <info@onesip.nl>
2026-08-01 21:33:50 +0200

    Assemble the high-resolution hero artwork

---
+++ src/assets/ruruHeroHd.ts
@@ -0,0 +1,37 @@
+import part01 from './ruruHeroHdPart01';
+import part02 from './ruruHeroHdPart02';
+import part03 from './ruruHeroHdPart03';
+import part04 from './ruruHeroHdPart04';
+import part05 from './ruruHeroHdPart05';
+import part06 from './ruruHeroHdPart06';
+import part07 from './ruruHeroHdPart07';
+import part08 from './ruruHeroHdPart08';
+import part09 from './ruruHeroHdPart09';
+import part10 from './ruruHeroHdPart10';
+import part11 from './ruruHeroHdPart11';
+import part12 from './ruruHeroHdPart12';
+import part13 from './ruruHeroHdPart13';
+import part14 from './ruruHeroHdPart14';
+import part15 from './ruruHeroHdPart15';
+import part16 from './ruruHeroHdPart16';
+
+const RURU_HERO_HD =
+  'data:image/webp;base64,' +
+  part01 +
+  part02 +
+  part03 +
+  part04 +
+  part05 +
+  part06 +
+  part07 +
+  part08 +
+  part09 +
+  part10 +
+  part11 +
+  part12 +
+  part13 +
+  part14 +
+  part15 +
+  part16;
+
+export default RURU_HERO_HD;
